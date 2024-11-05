e2915bad18dff21603ace0c9f18820685d15ec0b
jjb023 <gx20774@bristol.ac.uk>
2024-11-05 22:20:05 +0000

    Added market history csv folder creator

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ pip install csv
 pip install numpy
 pip install scikit-learn
 pip install matplotlib
+pip install yfinance
--- predictor.py
+++ predictor.py
@@ -2,6 +2,13 @@ import csv
 import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
+import yfinance as yf
+
 
 dates = []
 prices = []
+
+data = yf.download('AAPL', start='2024-01-01', end='2024-11-01')
+data.to_csv('AAPL.csv')
+
+with open('AAPL.csv', 'r') as csvfile:
