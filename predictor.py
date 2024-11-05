4b6abf6adaeeae94ebad2a05b99ba3eac62d99cd
jjb023 <gx20774@bristol.ac.uk>
2024-11-05 23:10:11 +0000

    Fix this

--- predictor.py
+++ predictor.py
@@ -3,12 +3,53 @@ import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
 import yfinance as yf
-
+from datetime import datetime
 
 dates = []
 prices = []
 
-data = yf.download('AAPL', start='2024-01-01', end='2024-11-01')
-data.to_csv('AAPL.csv')
+def get_data(filename):
+    with open(filename, 'r') as csvfile:
+        csvFileReader = csv.reader(csvfile)
+        #Skip first 3 rows
+        for i in range(3):
+            next(csvFileReader)
+        for row in csvFileReader:
+            date = datetime.strptime(row[0].split()[0], '%Y-%m-%d')
+            dates.append(date.toordinal())  # Convert to a numerical format
+            prices.append(float(row[1]))  # Make sure this corresponds to your intended column
+    return
 
-with open('AAPL.csv', 'r') as csvfile:
+def predict_prices(dates, prices, x):
+    dates = np.reshape(dates, (len(dates), 1))
+
+    svr_lin = SVR(kernel= 'linear', C= 1e3)
+    svr_poly = SVR(kernel= 'poly', C= 1e3, degree= 2)
+    svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1)
+    svr_lin.fit(dates, prices)
+    svr_poly.fit(dates, prices)
+    svr_rbf.fit(dates, prices)
+
+    plt.scatter(dates, prices, color= 'black', label= 'Data')
+    plt.plot(dates, svr_rbf.predict(dates), color= 'red', label= 'RBF model')
+    plt.plot(dates, svr_lin.predict(dates), color= 'green', label= 'Linear model')
+    plt.plot(dates, svr_poly.predict(dates), color= 'blue', label= 'Polynomial model')
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.title('Support Vector Regression')
+    plt.legend()
+    plt.show()
+
+    return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+
+get_data('AAPL.csv')
+print("Dates:", dates[:5])  # Print first 5 dates
+print("Prices:", prices[:5])  # Print first 5 prices
+
+
+x = np.array([[datetime(2024, 10, 5).toordinal()]])  # Example date
+predicted_price = predict_prices(dates, prices, x)
+
+print(predicted_price)
+
+
--- uptodatedataAAPL.py
+++ uptodatedataAAPL.py
@@ -1,4 +1,7 @@
 import yfinance as yf
 
-data = yf.download('AAPL', start='2024-01-01', end='2024-11-01')
+data = yf.download('AAPL', start='2024-10-01', end='2024-10-31')
+
+# Save data to csv file
 data.to_csv('AAPL.csv')
+
